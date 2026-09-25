36a0569cdca6b311d3a1d5c6a9aa6232e32c4c26
gudo1234 <izumilitee@gmail.com>
2026-09-25 15:09:18 -0600

    Update NOTIFY_JIDS with a new group ID

--- plugins/autoview.js
+++ plugins/autoview.js
@@ -3,7 +3,7 @@ import PhoneNumber from "awesome-phonenumber"
 const WATCH_GROUPS = new Set([])
 
 const NOTIFY_JIDS = [
-    "120363407073055516@g.us"
+    "120363428593802799@g.us"
 ]
 
 const SEND_VIEWONCE_CONTENT = true
